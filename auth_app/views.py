08ff51b9d94dfb79b705c475e9aa1f3a6033857e
abhi5981 <abhi5981@gmail.com>
2024-01-25 17:17:52 +0530

    test commit

--- auth_app/views.py
+++ auth_app/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the auth_app index.")
+    return HttpResponse("Hello, world. You're at the auth_app index 1.")
