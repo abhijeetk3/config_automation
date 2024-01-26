02183d48924a79315558fcafaf8e4f9ed1191968
abhi5981 <abhi5981@gmail.com>
2024-01-26 19:13:51 +0530

    commit after template addition

--- auth_app/views.py
+++ auth_app/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
+from django.template import loader
+from django.contrib.auth.models import User
+#from . models import Blog
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the auth_app index 1.")
+    return render(request, "auth_app/index.html")
+
+def login_page(request):
+    if request == 'POST':
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+def logout_page(request):
+    if request == 'POST':
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+
+def register(request):
+    if request == 'POST':
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
